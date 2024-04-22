194ba5e8906001d89b86dbb1216d9a15b2a6b70e
camilyalves <camily10.alves2007@gmail.com>
2024-04-22 17:24:40 -0400

    add questao18

---
+++ questao18.js
@@ -0,0 +1,6 @@
+/*
+Exercícios de forEach e map:
+Questão:   O que acontece se você chamar map() em um array vazio? 
+*/
+
+//Se você chamar map() em um array vazio, o resultado será um novo array vazio. Isso ocorre porque map() sempre retorna um novo array com o mesmo número de elementos que o array original, onde cada elemento é o resultado da função de callback aplicada ao elemento correspondente do array original. Quando o array original está vazio, não há elementos para mapear, então o novo array resultante também será vazio.
