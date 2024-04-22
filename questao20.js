dc7decce8a70788f832d58a637b35fbf825f6502
camilyalves <camily10.alves2007@gmail.com>
2024-04-22 17:25:11 -0400

    add questao20

---
+++ questao20.js
@@ -0,0 +1,6 @@
+/*
+Exercícios de forEach e map:
+Questão:    Qual é a utilidade de forEach() se ele não retornar nenhum valor?
+*/
+
+//embora forEach() não retorne um valor, ele ainda é uma ferramenta útil para iterar sobre os elementos de um array e executar operações para cada elemento, tornando o código mais claro, legível e fácil de entender.
