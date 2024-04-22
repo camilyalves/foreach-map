609ec69196e50b31b88a8e70b63daa047d630147
camilyalves <camily10.alves2007@gmail.com>
2024-04-22 17:23:02 -0400

    add questao11

---
+++ questao11.js
@@ -0,0 +1,7 @@
+/*
+Exercícios de forEach e map:
+Questão:    map() sempre retorna um novo array com o mesmo comprimento 
+que o array original?
+*/
+
+//sim, O método map() sempre retorna um novo array com o mesmo comprimento que o array original. Cada elemento do array original é mapeado para um elemento no novo array, e a função de callback é aplicada a cada elemento para determinar o valor correspondente no novo array.
