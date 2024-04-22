46134e19d0ded9ddbb3ab35f8d9240ac169215cb
camilyalves <camily10.alves2007@gmail.com>
2024-04-22 17:24:02 -0400

    add questao15

---
+++ questao15.js
@@ -0,0 +1,8 @@
+/*
+Exercícios de forEach e map:
+Questão:   Qual é o propósito principal do método map()? 
+*/
+
+//em JavaScript é criar um novo array com base em um array existente, aplicando uma função de callback a cada elemento desse array. Em outras palavras, map() é usado para transformar os elementos de um array, produzindo um novo array com os resultados dessas transformações.
+
+
