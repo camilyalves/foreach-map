129783884575aad13b00d5e885c0aa1ee8065d15
camilyalves <camily10.alves2007@gmail.com>
2024-04-22 17:25:29 -0400

    add questao21

---
+++ questao21.js
@@ -0,0 +1,7 @@
+/*
+Exercícios de forEach e map:
+Questão:    Existe alguma diferença de desempenho entre map() e 
+forEach()? 
+*/
+
+// pode ser insignificante na maioria dos casos de uso. A escolha entre eles deve ser feita com base na semântica e na intenção do código, priorizando a legibilidade e a clareza do código sobre pequenas diferenças de desempenho, a menos que você esteja lidando com volumes muito grandes de dados ou com cenários de desempenho crítico. Em tais casos, perfis de desempenho e testes específicos podem ajudar a determinar a melhor abordagem para o seu caso de uso.
