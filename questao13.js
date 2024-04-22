5e2a0fe0c0478892e9ea3140a7deae86ee3181b8
camilyalves <camily10.alves2007@gmail.com>
2024-04-22 17:23:32 -0400

    add questao13

---
+++ questao13.js
@@ -0,0 +1,8 @@
+/*
+Exercícios de forEach e map:
+Questão:    Quando você usaria map() em vez de forEach()?  
+*/
+
+//quando precisar transformar cada elemento de um array em algo diferente e criar um novo array com esses resultados. 
+
+//usaria map() sempre que precisar transformar elementos de um array e criar um novo array com essas transformações. Isso é particularmente útil quando você deseja manter uma abordagem funcional e imutável para lidar com dados em JavaScript.
